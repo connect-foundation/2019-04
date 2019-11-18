f1cff3d7edcf70c5b6fe668f56d125e5c6f52963
YukJiSoo <wltn3231@naver.com>
2019-11-18 18:51:10 +0900

    feat: 전체 페이지 기본 구조 구현
    전체 페이지 기본구조를 구현하고 router로 path를 설정했습니다.

--- cocode/src/App.js
+++ cocode/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import GlobalStyle from './components/GlobalStyle';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import { Home, DashBoard, Project } from './pages';
 
 function App() {
 	return (
-		<>
+		<Router>
 			<GlobalStyle />
-			<header>
-				<h1>Hello! Cocode!!</h1>
-				<h2>my react app!</h2>
-			</header>
-			<section></section>
-			<footer></footer>
-		</>
+			<Link to="/">홈</Link>
+			<Link to="/dashboard">대시보드</Link>
+			<Link to="/project">project</Link>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route path="/dashboard" component={DashBoard} />
+				<Route path="/project" component={Project} />
+			</Switch>
+		</Router>
 	);
 }
 
