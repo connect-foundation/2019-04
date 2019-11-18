60eb9db4bd7c1ca6a47338ae0c62740041cf16cf
YukJiSoo <wltn3231@naver.com>
2019-11-18 13:43:45 +0900

    feat: #76 글로벌 스타일 적용
    공통으로 적용될 글로벌 스타일을 적용했습니다.

--- cocode/src/App.js
+++ cocode/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import GlobalStyle from './components/GlobalStyle';
 
 function App() {
 	return (
-		<div>
+		<>
+			<GlobalStyle />
 			<header>
 				<h1>Hello! Cocode!!</h1>
 				<h2>my react app!</h2>
 			</header>
 			<section></section>
 			<footer></footer>
-		</div>
+		</>
 	);
 }
 
