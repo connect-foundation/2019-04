3e6598eb2c2dc2280b56136c404c6292151dc32b
lallaheeee <ooop0422@gmail.com>
2019-11-19 11:56:19 +0900

    feat: sign in 버튼 구현
    Sign in 버튼을 구현했습니다.

---
+++ cocode/src/containers/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as Styled from './style';
+
+function Hgeader() {
+	return (
+		<Styled.Header>
+			<Styled.SignInButton>Sign In</Styled.SignInButton>
+		</Styled.Header>
+	);
+}
+
+export default Header;
---
+++ cocode/src/containers/Header/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Header = styled.header``;
+
+const SignInButton = styled.button`
+	color: #ffffff;
+	font-size: 2rem;
+`;
+
+export { Header, SignInButton };
