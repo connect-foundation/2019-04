67ce7f16305889dedb150b6911ca41ec9e3a1692
lallaheeee <ooop0422@gmail.com>
2019-11-19 11:50:40 +0900

    chore: global 스타일에 속성 추가
    border와 outline 속성을 추가했습니다.

--- cocode/src/components/GlobalStyle/index.js
+++ cocode/src/components/GlobalStyle/index.js
@@ -6,6 +6,8 @@ const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
         margin: 0;
         padding: 0;
+        border: 0;
+        outline: none;
     }
     
     html, body {
