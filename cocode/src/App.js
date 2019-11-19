20d11aa16fea8b1e856d02b1569d1da8921244b5
Basiltoast <basiltoast@gmail.com>
2019-11-19 14:23:47 +0900

    feat: 기본 theme 생성
    기본 테마를 생성하였습니다.

--- cocode/src/App.js
+++ cocode/src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import GlobalStyle from './components/GlobalStyle';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import { Home, DashBoard, Project } from './pages';
+
+import { DEFAULT_THEME } from './constants/theme';
 
 function App() {
 	return (
 		<Router>
-			<GlobalStyle />
-			<Link to="/">홈</Link>
-			<Link to="/dashboard">대시보드</Link>
-			<Link to="/project">project</Link>
-			<Switch>
-				<Route exact path="/" component={Home} />
-				<Route path="/dashboard" component={DashBoard} />
-				<Route path="/project" component={Project} />
-			</Switch>
+			<ThemeProvider theme={DEFAULT_THEME}>
+				<GlobalStyle />
+				<Link to="/">홈</Link>
+				<Link to="/dashboard">대시보드</Link>
+				<Link to="/project">project</Link>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route path="/dashboard" component={DashBoard} />
+					<Route path="/project" component={Project} />
+				</Switch>
+			</ThemeProvider>
 		</Router>
 	);
 }
---
+++ cocode/src/constants/theme.js
@@ -0,0 +1,7 @@
+const DEFAULT_THEME = {
+	mainColor: '#2accf9',
+	backgroundColor: '#161419',
+	textColor: '#ffffff'
+};
+
+export { DEFAULT_THEME };
