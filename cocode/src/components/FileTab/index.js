b5025f3554ea8ea391e2c63318c74d2bd7db1061
hzoou <woojooo52@naver.com>
2019-11-21 23:06:15 +0900

    feat: #31 파일 탭의 닫기 버튼 클릭 시 해당 파일 닫힘
    프로젝트 페이지의 에디터 컨테이너에 속한 파일 탭의 닫기 버튼을 클릭하면
    해당 파일이 닫히는 기능을 구현했습니다.

--- cocode/src/components/FileTab/index.js
+++ cocode/src/components/FileTab/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import * as Styled from './style';
+import Close from './close.svg';
 
-function FileTab({ index, FileName, icon, type, className, onClick }) {
+function FileTab({ index, FileName, icon, type, className, onClick, onCloseClick }) {
     const handleTabClick = () => onClick(index);
+    const handleCloseClick = () => onCloseClick(index);
 
     return (
         <Styled.Tab onClick={handleTabClick} className={className}>
             <Styled.Icon src={icon} alt={type} />
             {FileName}
+            <Styled.Close src={Close} alt="close" onClick={handleCloseClick}/>
         </Styled.Tab>
     );
 }
