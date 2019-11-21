40a1177c5e4ea34b5862e9c37dfc2c1a57e1c95f
hzoou <woojooo52@naver.com>
2019-11-21 15:26:42 +0900

    chore: 메인 페이지 첫번째 섹션 디자인 수정
    메인 페이지 첫번째 섹션의 디자인을 전체적으로 수정했습니다.

--- cocode/src/containers/Header/style.js
+++ cocode/src/containers/Header/style.js
@@ -1,20 +1,24 @@
 import styled from 'styled-components';
 
 const Header = styled.header`
-	display: flex;
-	flex-direction: row;
-	justify-content: space-between;
-	align-items: center;
-
-	height: 11.2vh;
-
-	background-color: ${({ theme }) => theme.backgroundColor};
-	padding: 1rem 2rem;
+    & {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    
+        height: 12vh;
+    
+        background-color: ${({ theme }) => theme.backgroundColor};
+        padding: 2rem 2.3rem;
+    }
 `;
 
 const SignInButton = styled.button`
-	font-size: 1.5rem;
-	font-weight: 100;
+    & {
+        font-size: 1.4rem;
+        font-weight: 100;
+    }
 	
 	&:hover {
 	    font-weight: 400;
