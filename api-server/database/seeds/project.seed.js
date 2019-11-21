c2995818c2e1ffb04fb6ab4fe0daf444f4ee77f3
lallaheeee <ooop0422@gmail.com>
2019-11-21 15:18:43 +0900

    feat: #59 더미데이터 작성
    유저, 프로젝트, 파일의 더미데이터를 작성했습니다.
    js,html,css 등 코드의 내용을 스트링으로 저장하면 Eslint 에러가 나서
    eslintignore 설정에 해당 파일을 추가했습니다.

---
+++ api-server/database/seeds/project.seed.js
@@ -0,0 +1,32 @@
+const mock = [
+	{
+		_id: '5dd61901353f4858e1b5a9d3',
+		name: 'root',
+		description: 'directory',
+		author: '5dd6187b38a6e55825f1ec14',
+		rootDirectory: '5dd553be4561ae2bae9cb463'
+	},
+	{
+		_id: '5dd61901353f4858e1b5a9d0',
+		name: 'root',
+		description: 'directory',
+		author: '5dd6187b38a6e55825f1ec14',
+		rootDirectory: '5dd553be4561ae2bae9cb463'
+	},
+	{
+		_id: '5dd61901353f4858e1b5a9d1',
+		name: 'root',
+		description: 'directory',
+		author: '5dd6187b38a6e55825f1ec14',
+		rootDirectory: '5dd553be4561ae2bae9cb463'
+	},
+	{
+		_id: '5dd61901353f4858e1b5a9d2',
+		name: 'root',
+		description: 'directory',
+		author: '5dd6187b38a6e55825f1ec14',
+		rootDirectory: '5dd553be4561ae2bae9cb463'
+	}
+];
+
+export default mock;
---
+++ api-server/database/seeds/user.seed.js
@@ -0,0 +1,24 @@
+const mock = [
+	{
+		_id: '5dd6187b38a6e55825f1ec99',
+		usename: 'basiltoast',
+		projectIds: ['5dd553be4561ae2bae9cb463']
+	},
+	{
+		_id: '5dd61901353f4858e1b5a9d0',
+		usename: 'hzoou',
+		projectIds: ['5dd618936e83b45839f5adc4']
+	},
+	{
+		_id: '5dd61901353f4858e1b5a9d1',
+		usename: 'yukjisoo',
+		projectIds: ['5dd618936e83b45839f5adc5']
+	},
+	{
+		_id: '5dd61901353f4858e1b5a9d2',
+		usename: 'lallaheeee',
+		projectIds: ['5dd618936e83b45839f5adc6']
+	}
+];
+
+export default mock;
