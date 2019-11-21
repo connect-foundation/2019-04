763f1b18183e1b3d540742c99276f5e6572e93f6
hzoou <woojooo52@naver.com>
2019-11-21 19:18:47 +0900

    feat: 로그인 버튼 클릭 시 github 로그인 페이지로 이동
    현재 페이지의 주소를 github 로그인 페이지를 제공하는
    login api로 이동하는 기능을 구현했습니다.

--- cocode/src/components/Modal/index.js
+++ cocode/src/components/Modal/index.js
@@ -3,14 +3,18 @@ import * as Styled from './style';
 
 import CloseButton from 'components/CloseButton';
 
+const LOGIN_API_URL = 'http://localhost:3030/api/users/login';
+
 function Modal({ modalBody, onClose }) {
+	const handleClickLoginButton = () => window.location.href = LOGIN_API_URL;
+
 	return (
 		<Styled.ModalBackGround>
 			<Styled.Modal role="dialog">
 				<Styled.ModalHeader>
 					<CloseButton onClick={onClose} />
 				</Styled.ModalHeader>
-				<Styled.ModalBody>{modalBody}</Styled.ModalBody>
+				<Styled.ModalBody onClick={handleClickLoginButton}>{modalBody}</Styled.ModalBody>
 			</Styled.Modal>
 		</Styled.ModalBackGround>
 	);
