781d94fbed35043746b246018e5d79302b81ac7d
hzoou <woojooo52@naver.com>
2019-11-21 23:08:07 +0900

    feat: 파일 탭바에서 열려있는 파일 리스트 상태 관리
    프로젝트 페이지의 에디터 컨테이너의 파일 탭바에서
    열려있는 파일 리스트의 상태를 관리하기 위해 hook을 사용했습니다.
    열려있는 파일이 없는 경우에도 해당 탭바의 높이는 유지되어야 하므로
    min-height 스타일 속성을 추가했습니다.

---
+++ cocode/src/components/FileTabBar/index.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import * as Styled from './style';
+import FileTab from '../FileTab';
+
+const DEFAULT_OPENED_FILE_INDEX = 0;
+
+function FileTabBar() {
+	const openFileList = [
+		{
+			name: 'index.html',
+			type: 'html',
+			icon: 'https://cdn.jsdelivr.net/gh/PKief/vscode-material-icon-theme@master/icons/html.svg'
+		},
+		{
+			name: 'index.js',
+			type: 'js',
+			icon: 'https://cdn.jsdelivr.net/gh/PKief/vscode-material-icon-theme@master/icons/javascript.svg'
+		},
+		{
+			name: 'app.js',
+			type: 'js',
+			icon: 'https://cdn.jsdelivr.net/gh/PKief/vscode-material-icon-theme@master/icons/javascript.svg'
+		},
+		{
+			name: 'app.css',
+			type: 'css',
+			icon: 'https://cdn.jsdelivr.net/gh/PKief/vscode-material-icon-theme@master/icons/css.svg'
+		},
+	];
+
+	const [ clickedIndex, setClickedIndex ] = useState(DEFAULT_OPENED_FILE_INDEX);
+	//TODO 현재 더미데이터 fileList를 넣은 값으로, 추후 props에서 넘겨온 openFileList가 초기값이 될 예정입니다.
+	const [ openFiles, setOpenFiles ] = useState(openFileList);
+
+	const handleSetClickedIndex = (index) => setClickedIndex(index);
+	const handleCloseFile = (index) => setOpenFiles(openFiles.filter((file, i) => i !== index));
+
+	return (
+		<Styled.TabBar>
+			{openFiles.map(({ name, icon, type }, index) => {
+				return (
+					<FileTab
+						key={index}
+						index={index}
+						FileName={name}
+						icon={icon}
+						type={type}
+						className={index === clickedIndex ? 'clicked' : ''}
+						onClick={handleSetClickedIndex}
+						onCloseClick={handleCloseFile}
+					/>
+				);
+			})}
+		</Styled.TabBar>
+	);
+}
+
+export default FileTabBar;
