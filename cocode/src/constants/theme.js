5bf195ec07bb79f3ff2a5758a53c281c8af7136f
Basiltoast <basiltoast@gmail.com>
2019-11-22 09:58:30 +0900

    chore: project card theme 추가
    프로젝트 카드에서 사용할 테마를 추가하였습니다.

---
+++ cocode/src/constants/theme.js
@@ -0,0 +1,51 @@
+const DEFAULT_THEME = {
+	mainColor: '#2accf9',
+	mainOpaqueColor: '#2accf9b3',
+	backgroundColor: '#161419',
+	blackOpaqueColor: '#000000b3',
+	textColor: '#ffffff'
+};
+
+const BROWSER_THEME = {
+	browserHeaderBGColor: '#1d2022',
+	adressInputBGColor: '#000',
+	adressInputTextColor: '#fff',
+
+	browserHeignt: '88.8vh'
+};
+
+const TAB_CONTAINER_THEME = {
+	tabContainerBGColor: '#0e0f10',
+	tabContainerTitleColor: '#7E7E7E',
+	tabContainerFileTextColor: '#878788',
+	tabContainerFileHoverTextColor: '#fff',
+	tabContainerFileHoverBGColor: '#2accf944',
+
+	tabContainerTitleSize: '1.25rem',
+	tabContainerFileTextSize: '1rem',
+
+	tabContainerTitleWeight: '600'
+};
+
+const TAB_BAR_THEME = {
+	tabBarBGColor: '#1d2022',
+	tabBarItemBGColor: '#A5A6A7',
+	tabBarSelectedItemBGColor: '#fff'
+};
+
+const PROJECT_CARD_THEME = {
+	cardHeight: '9.5rem',
+	cardWidth: '24rem',
+	cardBackgroundColor: '#FFFFFF',
+	cardTitleFontSize: '2rem',
+	cardDescriptFontSize: '1.25rem',
+	cardTextColor: '#161419'
+};
+
+export {
+	DEFAULT_THEME,
+	BROWSER_THEME,
+	TAB_BAR_THEME,
+	TAB_CONTAINER_THEME,
+	PROJECT_CARD_THEME
+};
