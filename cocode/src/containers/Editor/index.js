b06beb88edbba9c1e030400e31c2dc4770c3feb3
YukJiSoo <wltn3231@naver.com>
2019-11-22 09:52:14 +0900

    feat: editor container에서 context 사용하도록 적용
    Project page의 하위 컴포넌트인 Editor에서 project context에 접근하도록
    구현했습니다.
    Editor는 MonacoEditor 컴포넌트에 code와 code change handler를 넘겨 하위
    컴포넌트에서 상위 컴포넌트의 구체적인 내용을 앞 필요 없도록 구현했습니다.

--- cocode/src/containers/Editor/index.js
+++ cocode/src/containers/Editor/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import * as Styled from './style';
 
 import FileTabBar from 'components/FileTabBar';
 import MonacoEditor from 'components/MonacoEditor';
 
+import ProjectContext from 'contexts/ProjectContext';
+import { updateCodeActionCreator } from 'actions/Project';
+
 function Editor() {
+	const { project, dispatchProject } = useContext(ProjectContext);
+	const { code } = project;
+
+	const handleChangeCode = (_, changedCode) => {
+		const updateCodeAction = updateCodeActionCreator(changedCode);
+		dispatchProject(updateCodeAction);
+	};
+
 	return (
 		<Styled.Editor>
 			<FileTabBar />
-			<MonacoEditor className="Stretch-width" />
+			<MonacoEditor
+				code={code}
+				onChange={handleChangeCode}
+				className="Stretch-width"
+			/>
 		</Styled.Editor>
 	);
 }
