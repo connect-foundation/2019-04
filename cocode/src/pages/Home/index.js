bbe9ec505b11190dc9f05a11b80988da11a38090
hzoou <woojooo52@naver.com>
2019-11-23 22:32:19 +0900

    feat: 마우스 휠 이벤트 콜백 구현
    스크롤을 이동시켜 위로 마우스 휠을 하면 이전 섹션을,
    아래로 마우스 퓔을 하면 다음 섹션을 노출하는 함수를 구현했습니다.
    메인 페이지에서 해당 함수를 마우스 휠 이벤트 콜백으로 사용했습니다.

--- cocode/src/pages/Home/index.js
+++ cocode/src/pages/Home/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from 'containers/Header';
 import Main from 'containers/Main';
 import AboutCocode from 'containers/AboutCocode';
 import AboutUs from 'containers/AboutUs';
 
+import addWheelEvent from 'utils/addWheelEvent';
+
 function Home() {
+	useEffect(addWheelEvent, []);
+
 	return (
 		<>
 			<Header />
