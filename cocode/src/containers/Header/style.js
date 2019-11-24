515ce700ddcb578ec51bf08b72edfc3e862fca7f
YukJiSoo <wltn3231@naver.com>
2019-11-24 16:13:50 +0900

    feat: header container에 history category 추가
    Header에 history page로 이동하는 history category를 추가했습니다.
    Header의 우측에 나타나는 부분을 div로 감싸주어 항상 우측에 나타나도록
    수정했습니다.

--- cocode/src/containers/Header/style.js
+++ cocode/src/containers/Header/style.js
@@ -1,28 +1,43 @@
 import styled from 'styled-components';
 
 const Header = styled.header`
-    & {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-    
-        height: 12vh;
-    
-        background-color: ${({ theme }) => theme.backgroundColor};
-        padding: 2rem 2.3rem;
-    }
-`;
+	& {
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		align-items: center;
 
-const SignInButton = styled.button`
-    & {
-        font-size: 1.4rem;
-        font-weight: 100;
-    }
-	
-	&:hover {
-	    font-weight: 400;
+		height: 12vh;
+
+		background-color: ${({ theme }) => theme.backgroundColor};
+		padding: 2rem 2.3rem;
 	}
 `;
 
-export { Header, SignInButton };
+const HeaderCategory = styled.button`
+	& {
+		margin-left: 1.5rem;
+
+		font-size: 1.4rem;
+		font-weight: 100;
+	}
+`;
+
+const HeaderRightSideArea = styled.div`
+	& {
+		margin-left: auto;
+	}
+`;
+
+const SignInButton = styled.button`
+	& {
+		font-size: 1.4rem;
+		font-weight: 100;
+	}
+
+	&:hover {
+		font-weight: 400;
+	}
+`;
+
+export { Header, SignInButton, HeaderCategory, HeaderRightSideArea };
