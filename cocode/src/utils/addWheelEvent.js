be9ef7232829e2e37049cb4242fa03155e3d5328
Basiltoast <basiltoast@gmail.com>
2019-11-25 18:25:45 +0900

    refactor: 메인 페이지 scroll event 리팩토링
    메인 페이지의 wheel, scroll 이벤트를 리팩토링하였습니다.
    wheel 이벤트와 scroll 이벤트를 debounce 기법을 적용하여 이벤트가 한
    번만 실행되도록 하였습니다.

--- cocode/src/utils/addWheelEvent.js
+++ cocode/src/utils/addWheelEvent.js
@@ -1,45 +1,18 @@
-let marker = true;
-let scroll = true;
-let delta;
-let counter1 = 0;
-let counter2;
+let scroll = false;
 
-function wheel (e) {
-    counter1++;
-    delta = e.deltaY;
-    if (marker) wheelStart();
-};
-
-function wheelStart () {
-    marker = false;
-    wheelAct();
-};
-
-function wheelAct () {
-    counter2 = counter1;
-    setTimeout(() => {
-        if (counter2 === counter1) return wheelEnd();
-        if (scroll) return scrollMove();
-        return wheelAct();
-    }, 10);
-};
-
-function wheelEnd() {
-    marker = true;
-    scroll = true;
-    counter1 = 0;
-    counter2 = false;
-};
-
-function scrollMove() {
-    scroll = false;
-    if (delta > 0) window.scrollBy(0, -window.innerHeight);
-    else if (delta < 0) window.scrollBy(0, window.innerHeight);
-    wheelAct();
-};
+function handleWheelScroll(e) {
+	if (scroll) return;
+	scroll = true;
+	if (e.deltaY < 0) window.scrollBy(0, -window.innerHeight);
+	else window.scrollBy(0, window.innerHeight);
+	wheelTimer = setTimeout(handleWheelAble, 1000);
+}
+function handleWheelAble() {
+	scroll = false;
+}
 
 function addWheelEvent() {
-    window.addEventListener('wheel', wheel);
+	window.addEventListener('wheel', handleWheelScroll);
 }
 
 export default addWheelEvent;
