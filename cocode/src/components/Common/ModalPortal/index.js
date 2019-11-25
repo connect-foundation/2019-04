ef111effcba84e27b6a492509218c41bf5b35e72
hzoou <woojooo52@naver.com>
2019-11-25 13:38:06 +0900

    fix: #139 스크롤 버그 수정
    모달이 닫힐때 root 영역의 overflow를 initial 속성으로 변경하는 코드를 추가하여
    모달이 띄워지고 나면 스크롤이 동작하지 않는 버그를 수정했습니다.

--- cocode/src/components/Common/ModalPortal/index.js
+++ cocode/src/components/Common/ModalPortal/index.js
@@ -1,8 +1,6 @@
 import ReactDOM from 'react-dom';
 
 const ModalPortal = ({ children }) => {
-	const root = document.getElementById('root');
-	root.style.overflow = 'hidden';
 	const modalElement = document.getElementById('modal');
 	return ReactDOM.createPortal(children, modalElement);
 };
