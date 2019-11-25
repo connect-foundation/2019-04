e7e7742df61fdd6d0abeae7be3eaf967323c6913
hzoou <woojooo52@naver.com>
2019-11-25 17:54:56 +0900

    feat: #24 프로젝트 페이지 좌측 탭바 UI 구현
    프로젝트 페이지 좌측의 탭바 UI를 구현했습니다.

--- cocode/src/components/Project/TabBar/index.js
+++ cocode/src/components/Project/TabBar/index.js
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as Styled from './style';
 
-import ProjectIcon from 'components/Project/ProjectIcon';
+import TabIcon from 'components/Project/TabIcon';
+
+import Info from './info.svg';
+import Explorer from './explorer.svg';
+import Dependency from './dependency.svg';
+import Live from './live.svg';
 
 function TabBar({ theme }) {
+	const [clickedIndex, setClickedIndex] = useState(0);
+	const handleSetClickedIndex = index => setClickedIndex(index);
+
+	const tabIcons = [
+		{
+			name: 'info',
+			icon: Info,
+		},
+		{
+			name: 'explorer',
+			icon: Explorer,
+		},
+		{
+			name: 'dependency',
+			icon: Dependency,
+		},
+		{
+			name: 'live',
+			icon: Live,
+		}
+	];
+
 	return (
-		<Styled.TabBar backGroundColor={theme.tabBarBGColor}>
-			<Styled.TabBarItem>
-				<ProjectIcon fillColor={theme.tabBarSelectedItemBGColor} />
-			</Styled.TabBarItem>
+		<Styled.TabBar tabBarBGColor={theme.tabBarBGColor}>
+			{tabIcons.map(({ name, icon }, index) => {
+				return (
+					<TabIcon
+						theme={theme}
+						key={index}
+						index={index}
+						name={name}
+						icon={icon}
+						onClick={handleSetClickedIndex}
+						clicked={index === clickedIndex}
+					/>
+				);
+			})}
 		</Styled.TabBar>
 	);
 }
--- cocode/src/components/Project/TabBar/style.js
+++ cocode/src/components/Project/TabBar/style.js
@@ -2,18 +2,14 @@ import styled from 'styled-components';
 
 const TabBar = styled.nav`
 	& {
+		min-width: 4rem;
 		display: inline-flex;
 		flex-direction: column;
 		justify-content: flex-start;
-
+		align-items: center;
+		
 		background-color: ${({ tabBarBGColor }) => tabBarBGColor};
 	}
 `;
 
-const TabBarItem = styled.button`
-	& {
-		margin: 1rem;
-	}
-`;
-
-export { TabBar, TabBarItem };
+export { TabBar };
