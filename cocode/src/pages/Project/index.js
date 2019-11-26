3d0aab53c044815de538ec212673c5f7bb02830c
YukJiSoo <wltn3231@naver.com>
2019-11-26 21:13:50 +0900

    chore: project context 사용하는 곳에 변경사항 적용
    project dummy data 추가에 따라 변경된 reducer를 다른 컴포넌트에
    적용해주었습니다.

--- cocode/src/pages/Project/index.js
+++ cocode/src/pages/Project/index.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useEffect } from 'react';
+import React, { useReducer, useEffect, useState } from 'react';
 import * as Styled from './style';
 
 import Header from 'containers/Common/Header';
@@ -14,11 +14,14 @@ import { fetchProjectActionCreator } from 'actions/Project';
 import { TAB_BAR_THEME } from 'constants/theme';
 
 function Project() {
+	// temp state : custom hook 만들면 대체할 예정
+	const [isFetched, setIsFetched] = useState(false);
 	const [project, dispatchProject] = useReducer(ProjectReducer, {});
 
 	const handleFetchProject = () => {
 		const fetchProjectAction = fetchProjectActionCreator();
 		dispatchProject(fetchProjectAction);
+		setIsFetched(true);
 	};
 
 	useEffect(handleFetchProject, []);
@@ -26,16 +29,18 @@ function Project() {
 	return (
 		<ProjectContext.Provider value={{ project, dispatchProject }}>
 			<Header />
-			<Styled.Main>
-				<TabBar theme={TAB_BAR_THEME} />
-				<ExplorerTab />
-				<Editor />
-				<BrowserV1
-					code={project.code}
-					id="coconut-root"
-					className="Project-main-stretch"
-				/>
-			</Styled.Main>
+			{isFetched && (
+				<Styled.Main>
+					<TabBar theme={TAB_BAR_THEME} />
+					<ExplorerTab />
+					<Editor />
+					<BrowserV1
+						code={project.files[project.entry].contents}
+						id="coconut-root"
+						className="Project-main-stretch"
+					/>
+				</Styled.Main>
+			)}
 		</ProjectContext.Provider>
 	);
 }
