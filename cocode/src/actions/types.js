aa582ad8d9111c87822bc3573d0a6714b5f84c52
YukJiSoo <wltn3231@naver.com>
2019-11-27 10:17:48 +0900

    feat: update file name action 정의
    update file name action에 대한 type을 정의하고  action creator를 구현했습니다.

--- cocode/src/actions/types.js
+++ cocode/src/actions/types.js
@@ -2,5 +2,6 @@
 const UPDATE_CODE = 'updateCode';
 const FETCH_PROJECT = 'fetchProject';
 const SELECT_FILE = 'selectFile';
+const UPDATE_FILE_NAME = 'updateFileName';
 
-export { UPDATE_CODE, FETCH_PROJECT, SELECT_FILE };
+export { UPDATE_CODE, FETCH_PROJECT, SELECT_FILE, UPDATE_FILE_NAME };
