e4911ff9ad24fa5ca357388e8f3339eea10da635
Basiltoast <basiltoast@gmail.com>
2019-11-27 10:15:43 +0900

    feat: #36 프로젝트 정보 탭 컨테이너 구현
    프로젝트 정보 탭 컨테이너를 구현하였습니다.

---
+++ cocode/src/constants/theme.js
@@ -0,0 +1,68 @@
+const DEFAULT_THEME = {
+	mainColor: '#2accf9',
+	mainOpaqueColor: '#2accf9b3',
+	backgroundColor: '#161419',
+	blackOpaqueColor: '#000000b3',
+	textColor: '#ffffff',
+
+	exceptHeaderHeignt: '88vh',
+	headerHeignt: '12vh'
+};
+
+const BROWSER_THEME = {
+	browserHeaderBGColor: '#1d2022',
+	adressInputBGColor: '#000',
+	adressInputTextColor: '#fff',
+
+	browserHeignt: '88vh'
+};
+
+const TAB_CONTAINER_THEME = {
+	tabContainerBGColor: '#0e0f10',
+	tabContainerTitleColor: '#7E7E7E',
+	tabContainerFileTextColor: '#878788',
+	tabContainerFileHoverTextColor: '#fff',
+	tabContainerFileHoverBGColor: '#2accf944',
+
+	tabContainerTitleSize: '1.25rem',
+	tabContainerFileTextSize: '1rem',
+
+	tabContainerTitleWeight: '600'
+};
+
+const TAB_BAR_THEME = {
+	tabBarBGColor: '#1E2022',
+	tabBarSelectedItemBGColor: '#cfcfcf'
+};
+
+const PROJECT_CARD_THEME = {
+	cardHeight: '9.5rem',
+	cardWidth: '24rem',
+	cardBackgroundColor: '#FFFFFF',
+	cardTitleFontSize: '1.5rem',
+	cardDescriptFontSize: '1.25rem',
+	cardTextColor: '#161419',
+	buttonBackgroundColor: '#232e36'
+};
+
+const INFO_TAB_THEME = {
+	infoPadding: '0.7rem 1rem',
+	infoDeleteButtonBGColor: '#880000',
+	infoDeleteButtonBGCOlorHover: '#ee0000',
+	infoModifySVGSize: '1rem',
+	infoModifySVGFilter: 'invert(0.455)',
+	infoModifySVGFilterHover: 'invert(0.9)',
+	infoProjectNameFontSize: '1.5rem',
+	infoProjectDescriptionFontSize: '1.35rem',
+	infoProjectDescriptionFontStyle: 'italic',
+	infoProjectAuthorFontSize: '1.2rem'
+};
+
+export {
+	DEFAULT_THEME,
+	BROWSER_THEME,
+	TAB_BAR_THEME,
+	TAB_CONTAINER_THEME,
+	PROJECT_CARD_THEME,
+	INFO_TAB_THEME
+};
