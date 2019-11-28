dd1e947cbed30bec308974607eb4694702d74c57
lallaheeee <ooop0422@gmail.com>
2019-11-28 18:07:29 +0900

    fix: 기본 app.js에 바디 파서 옵션 추가 및 코드 정리
    조금더 깔끔한 express를 위해 DB 옵션과 CORS 옵션을 config로 분리했습니다.
    또한 라우터에서 req.body 값을 얻기 위해 설정을 추가했습니다.

--- api-server/src/app.js
+++ api-server/src/app.js
@@ -3,11 +3,9 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
-
-import { PORT, DATABASE_URI } from './config';
-
 import passport from './middlewares/passport';
 import apiRouter from './routes';
+import { PORT, DATABASE_URI, CORS_OPTION, MONGO_OPTION } from './config';
 
 const app = express();
 
@@ -15,20 +13,13 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log('connected to Mongo'));
 
-mongoose.connect(DATABASE_URI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true
-});
-
-const corsOption = {
-	origin: (origin, callback) => callback(null, true),
-	exposedHeaders: ['Set-Cookie'],
-	credentials: true
-};
+mongoose.connect(DATABASE_URI, MONGO_OPTION);
 
 app.use(morgan('dev'));
-app.use(cors(corsOption));
+app.use(cors(CORS_OPTION));
 app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize());
 
 app.use('/api', apiRouter);
