3582217729952ba9f2b50a0407d5abdacabb0781
lallaheeee <ooop0422@gmail.com>
2019-11-28 10:51:36 +0900

    feat: 대시보드 초기 사용자 프로젝트 목록 조회 API 요청 구현
    초기에 프로젝트 목록을 조회하는 API 요청을 구현했습니다.

--- cocode/src/apis/DashBoard.js
+++ cocode/src/apis/DashBoard.js
@@ -1,7 +1,6 @@
 import { API } from 'config';
 
 function getCoconutsAPICreator(username) {
-	console.log(`${API.users}/${username}/projects`);
 	return {
 		url: `${API.users}/${username}/projects`,
 		method: 'get'
