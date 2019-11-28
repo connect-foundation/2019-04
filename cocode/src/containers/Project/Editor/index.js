0d3d04682687cb290a730a9fd202278d9201ae47
BasilToast <33782602+BasilToast@users.noreply.github.com>
2019-11-29 01:29:32 +0900

    feat: 파일 선택시 이벤트 추가
    파일 익스플로러에서 파일 선택시, 해당파일의 정보가 에디터에 표시되고,
    수정하면 해당 파일에 정보가 수정되도록 이벤트를 추가하였습니다.

--- cocode/src/containers/Project/Editor/index.js
+++ cocode/src/containers/Project/Editor/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import * as Styled from './style';
 
 import FileTabBar from 'components/Project/FileTabBar';
@@ -10,22 +10,31 @@ import { updateCodeActionCreator } from 'actions/Project';
 function Editor() {
 	const { project, dispatchProject } = useContext(ProjectContext);
 	const { files, entry, selectedFileId } = project;
-	const entryCode = files[entry].contents;
+	const [code, setCode] = useState('');
 
-	const handleChangeCode = (_, changedCode) => {
-		const updateCodeAction = updateCodeActionCreator({
-			selectedFileId,
-			changedCode
-		});
-		dispatchProject(updateCodeAction);
+	const handleOnChange = (_, changedCode) => {
+		setCode(changedCode);
 	};
+
+	useEffect(() => {
+		if (selectedFileId === undefined) return;
+		setCode(files[selectedFileId].contents);
+	}, [selectedFileId]);
+
+	useEffect(() => {
+		if (code === '' || code === undefined) return;
+		dispatchProject(updateCodeActionCreator({
+			selectedFileId,
+			changedCode: code
+		}));
+	}, [code]);
 
 	return (
 		<Styled.Editor>
 			<FileTabBar />
 			<MonacoEditor
-				code={entryCode}
-				onChange={handleChangeCode}
+				code={code}
+				onChange={handleOnChange}
 				className="Stretch-width"
 			/>
 		</Styled.Editor>
