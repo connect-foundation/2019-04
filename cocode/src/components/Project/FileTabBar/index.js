39d4f7089b02ca49968b5f0e6b7e4364f5a12dcb
YukJiSoo <wltn3231@naver.com>
2019-11-29 04:43:22 +0900

    chore: file tab bar의 file list state 수정
    파일탭바의 파일 리스트가 project context의 현재 열려있는 파일의
    목록을 보고 관리되도록 수정했습니다.
    추후에 파일 탭을 닫았을 때의 처리를 해줄 필요가 있습니다.

--- cocode/src/components/Project/FileTabBar/index.js
+++ cocode/src/components/Project/FileTabBar/index.js
@@ -1,40 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import * as Styled from './style';
 import FileTab from '../FileTab';
+
+import FileImagesSrc from 'constants/fileImagesSrc';
+
+import ProjectContext from 'contexts/ProjectContext';
 
 const DEFAULT_OPENED_FILE_INDEX = 0;
 
 function FileTabBar() {
-	const openFileList = [
-		{
-			name: 'index.html',
-			type: 'html',
-			icon:
-				'https://cdn.jsdelivr.net/gh/PKief/vscode-material-icon-theme@master/icons/html.svg'
-		},
-		{
-			name: 'index.js',
-			type: 'js',
-			icon:
-				'https://cdn.jsdelivr.net/gh/PKief/vscode-material-icon-theme@master/icons/javascript.svg'
-		},
-		{
-			name: 'app.js',
-			type: 'js',
-			icon:
-				'https://cdn.jsdelivr.net/gh/PKief/vscode-material-icon-theme@master/icons/javascript.svg'
-		},
-		{
-			name: 'app.css',
-			type: 'css',
-			icon:
-				'https://cdn.jsdelivr.net/gh/PKief/vscode-material-icon-theme@master/icons/css.svg'
-		}
-	];
+	const { project } = useContext(ProjectContext);
+	const { files, selectedFileList } = project;
 
 	const [clickedIndex, setClickedIndex] = useState(DEFAULT_OPENED_FILE_INDEX);
 	//TODO 현재 더미데이터 fileList를 넣은 값으로, 추후 props에서 넘겨온 openFileList가 초기값이 될 예정입니다.
-	const [openFiles, setOpenFiles] = useState(openFileList);
+	const [openFiles, setOpenFiles] = useState(selectedFileList);
 
 	const handleSetClickedIndex = index => setClickedIndex(index);
 	const handleCloseFile = index =>
@@ -42,20 +22,23 @@ function FileTabBar() {
 
 	return (
 		<Styled.TabBar>
-			{openFiles.map(({ name, icon, type }, index) => {
-				return (
-					<FileTab
-						key={index}
-						index={index}
-						FileName={name}
-						icon={icon}
-						type={type}
-						className={index === clickedIndex ? 'clicked' : ''}
-						onClick={handleSetClickedIndex}
-						onCloseClick={handleCloseFile}
-					/>
-				);
-			})}
+			{openFiles
+				.map(path => files[path])
+				.map(({ name, type }, index) => {
+					const icon = FileImagesSrc[type];
+					return (
+						<FileTab
+							key={index}
+							index={index}
+							FileName={name}
+							icon={icon}
+							type={type}
+							className={index === clickedIndex ? 'clicked' : ''}
+							onClick={handleSetClickedIndex}
+							onCloseClick={handleCloseFile}
+						/>
+					);
+				})}
 		</Styled.TabBar>
 	);
 }
--- cocode/src/dummy/Project.js
+++ cocode/src/dummy/Project.js
@@ -118,47 +118,50 @@ const project = {
 			name: 'root',
 			type: 'directory',
 			child: [
-				'5dd553be4561ae2bae9cb45f',
-				'5dd553be4561ae2bae9cb461',
-				'5dd553be4561ae2bae9cb462'
+				'5dd553be4561ae2bae9cb45f'
+				// '5dd553be4561ae2bae9cb461',
+				// '5dd553be4561ae2bae9cb462'
 			]
 		},
 		{
 			_id: '5dd553be4561ae2bae9cb45f',
 			name: 'src',
 			type: 'directory',
-			child: ['5dd553be4561ae2bae9cb45d', '5dd553be4561ae2bae9cb45e']
+			child: [
+				'5dd553be4561ae2bae9cb45d'
+				// '5dd553be4561ae2bae9cb45e'
+			]
 		},
 		{
 			name: 'index.js',
 			type: 'js',
 			contents: getTemplate('version1'),
 			_id: '5dd553be4561ae2bae9cb45d'
-		},
-		{
-			name: 'style.css',
-			type: 'css',
-			contents: getTemplate('css'),
-			_id: '5dd553be4561ae2bae9cb45e'
-		},
-		{
-			_id: '5dd553be4561ae2bae9cb461',
-			name: 'public',
-			type: 'directory',
-			child: ['5dd553be4561ae2bae9cb460']
-		},
-		{
-			name: 'index.html',
-			type: 'html',
-			contents: getTemplate('html'),
-			_id: '5dd553be4561ae2bae9cb460'
-		},
-		{
-			name: 'package.json',
-			type: 'npm',
-			contents: getTemplate('package'),
-			_id: '5dd553be4561ae2bae9cb462'
 		}
+		// {
+		// 	name: 'style.css',
+		// 	type: 'css',
+		// 	contents: getTemplate('css'),
+		// 	_id: '5dd553be4561ae2bae9cb45e'
+		// },
+		// {
+		// 	_id: '5dd553be4561ae2bae9cb461',
+		// 	name: 'public',
+		// 	type: 'directory',
+		// 	child: ['5dd553be4561ae2bae9cb460']
+		// },
+		// {
+		// 	name: 'index.html',
+		// 	type: 'html',
+		// 	contents: getTemplate('html'),
+		// 	_id: '5dd553be4561ae2bae9cb460'
+		// },
+		// {
+		// 	name: 'package.json',
+		// 	type: 'npm',
+		// 	contents: getTemplate('package'),
+		// 	_id: '5dd553be4561ae2bae9cb462'
+		// }
 	]
 };
 
