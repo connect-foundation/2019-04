012dc4bd435d8cf698359fdd48b19c1685c30e99
hzoou <woojooo52@naver.com>
2019-11-28 18:41:40 +0900

    feat: #159 탭 컨테이너와 탭바 연동
    clickedTabIndex 상태값으로 탭바와 탭 컨테이너가 상태를 공유하며,
    탭 아이콘 클릭마다 탭 컨테이너가 변경되도록 연동하였습니다.

--- cocode/src/pages/Project/index.js
+++ cocode/src/pages/Project/index.js
@@ -2,8 +2,8 @@ import React, { useReducer, useEffect, useState } from 'react';
 import * as Styled from './style';
 
 import Header from 'containers/Common/Header';
-import TabBar from 'components/Project/TabBar';
-import ExplorerTab from 'containers/Project/ExplorerTab';
+import TabBar from 'containers/Project/TabBar';
+import TabContainer from 'containers/Project/TabContainer';
 import Editor from 'containers/Project/Editor';
 import BrowserV1 from 'components/Project/BrowserV1';
 import { SplitPaneContainer } from 'components/Common/SplitPane';
@@ -14,9 +14,14 @@ import { fetchProjectActionCreator } from 'actions/Project';
 
 import { TAB_BAR_THEME } from 'constants/theme';
 
+const DEFAULT_CLICKED_TAB_INDEX = 0;
+
 function Project() {
 	// temp state : custom hook 만들면 대체할 예정
 	const [isFetched, setIsFetched] = useState(false);
+	const [clickedTabIndex, setClickedTabIndex] = useState(
+		DEFAULT_CLICKED_TAB_INDEX
+	);
 	const [project, dispatchProject] = useReducer(ProjectReducer, {});
 
 	const handleFetchProject = () => {
@@ -28,13 +33,20 @@ function Project() {
 	useEffect(handleFetchProject, []);
 
 	return (
-		<ProjectContext.Provider value={{ project, dispatchProject }}>
+		<ProjectContext.Provider
+			value={{
+				project,
+				dispatchProject,
+				clickedTabIndex,
+				setClickedTabIndex
+			}}
+		>
 			<Header />
 			{isFetched && (
 				<Styled.Main>
 					<TabBar theme={TAB_BAR_THEME} />
 					<SplitPaneContainer split="vertical" defaultSize="20vw">
-						<ExplorerTab />
+						<TabContainer />
 						<SplitPaneContainer split="vertical" defaultSize="40vw">
 							<Editor />
 							<BrowserV1 code={project.code} id="coconut-root" />
