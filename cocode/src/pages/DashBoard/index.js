eaeb42ad04cac194742404ebd1066cb59dc09af7
lallaheeee <ooop0422@gmail.com>
2019-12-03 01:29:58 +0900

    fix: 대시보드에 usereduce를 사용하여 상태 관리 변경
    기존에 useState로 관리하던 상태를
    관리해야하는 액션이 추가되어 useReduce로 변경했습니다.

--- cocode/src/pages/DashBoard/index.js
+++ cocode/src/pages/DashBoard/index.js
@@ -1,14 +1,18 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useReducer } from 'react';
 import ProjectCardList from 'containers/DashBoard/ProjectCardList';
 import Header from 'containers/Common/Header';
+import { UserContext, DashBoardContext } from 'contexts';
+import DashBoardReducer from 'reducers/DashboardReducer';
 import useFetch from 'hooks/useFetch';
 import { getCoconutsAPICreator } from 'apis/DashBoard';
-import { DashBoardContext, UserContext } from 'contexts';
+import { fetchCoconutActionCreator } from 'actions/Dashboard';
 
 function DashBoard() {
 	const { user } = useContext(UserContext);
-	const [coconuts, setCoconuts] = useState([]);
-	const [isFetched, setIsFetched] = useState(false);
+	const [{ coconuts, isFetched }, dispatch] = useReducer(DashBoardReducer, {
+		coconuts: [],
+		isFetched: false
+	});
 	const [{ data, loading, error }, setRequest] = useFetch({});
 
 	useEffect(() => {
@@ -16,10 +20,7 @@ function DashBoard() {
 	}, [user]);
 
 	useEffect(() => {
-		if (!isFetched && data) {
-			setCoconuts(data);
-			setIsFetched(true);
-		}
+		if (!isFetched && data) dispatch(fetchCoconutActionCreator(data));
 	}, [data]);
 
 	//TODO loading 컴포넌트 만들기
