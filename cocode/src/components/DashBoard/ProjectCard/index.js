3ec50abe24dc53934587af66c337d481aba66d83
lallaheeee <ooop0422@gmail.com>
2019-12-02 23:24:29 +0900

    feat: 프로젝트 카드의 코코넛 이름 변경시 상태 변경 이벤 추가 트
    지수님 감사합니다.

--- cocode/src/components/DashBoard/ProjectCard/index.js
+++ cocode/src/components/DashBoard/ProjectCard/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import * as Styled from './style';
 import DropDownMenu from 'components/Common/DropDownMenu';
 import moment from 'moment';
+import { KEY_CODE_ENTER } from 'constants/keyCode';
+import {
+	selectAllTextAboutFocusedDom,
+	changeDivEditable
+} from 'utils/domControl';
 
 //TODO onClick시 api 요청 이벤트 핸들러 추가
 const defaultMenuItems = [
@@ -30,14 +35,47 @@ function MenuButton({ onClick }) {
 }
 
 function ProjectCard({ name, updatedAt, menuItems = defaultMenuItems }) {
+	const renameMenu = [
+		{
+			value: 'rename',
+			onClick: () => handleEditCoconutNameStart()
+		}
+	];
+
+	const [_, setCoconutName] = useState(name);
+	const nameInput = useRef(null);
+
+	const handleEditCoconutNameStart = () => {
+		changeDivEditable(nameInput.current, true);
+	};
+	const handleEditCoconutNameEnd = () => {
+		const changedName = nameInput.current.value;
+		setCoconutName(changedName);
+		console.log('end');
+
+		nameInput.current.contentEditable = false;
+	};
+
+	const handleKeyDown = ({ keyCode }) => {
+		if (keyCode === KEY_CODE_ENTER) {
+			handleEditCoconutNameEnd();
+		}
+	};
+
 	return (
 		<Styled.ProjectArticle>
-			<Styled.ProjectTitle>{name}</Styled.ProjectTitle>
+			<Styled.ProjectTitle
+				ref={nameInput}
+				onFocus={selectAllTextAboutFocusedDom}
+				onKeyDown={handleKeyDown}
+			>
+				{name}
+			</Styled.ProjectTitle>
 			<Styled.ProjectDescription>
 				<Styled.ProjectTimeLabel>
 					Edited {moment(updatedAt).fromNow()}
 				</Styled.ProjectTimeLabel>
-				<DropDownMenu menuItems={menuItems}>
+				<DropDownMenu menuItems={renameMenu}>
 					<MenuButton />
 				</DropDownMenu>
 			</Styled.ProjectDescription>
