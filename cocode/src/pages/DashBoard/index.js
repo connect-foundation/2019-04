8d1c112a4917c11e64c2de3fb850642f943a64a4
lallaheeee <ooop0422@gmail.com>
2019-12-04 22:24:48 +0900

    fix: 대시보드에서 무한 로딩으로 initial fetch 상태 추가

--- cocode/src/pages/DashBoard/index.js
+++ cocode/src/pages/DashBoard/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 import ProjectCardList from 'containers/DashBoard/ProjectCardList';
 import Header from 'containers/Common/Header';
 import { UserContext, DashBoardContext } from 'contexts';
@@ -10,13 +10,17 @@ import { fetchCoconutActionCreator } from 'actions/Dashboard';
 function DashBoard() {
 	const { user } = useContext(UserContext);
 	const [coconuts, dispatchDashboard] = useReducer(DashBoardReducer, []);
+	const [initialFetch, setInitialFetch] = useState(false);
 	const [{ data, loading, error }, setRequest] = useFetch({});
 
 	useEffect(() => {
+		if (initialFetch) return;
 		if (user) setRequest(getCoconutsAPICreator(user.username));
-		if (data) dispatchDashboard(fetchCoconutActionCreator(data));
+		if (data) {
+			dispatchDashboard(fetchCoconutActionCreator(data));
+			setInitialFetch(true);
+		}
 	}, [user, data]);
-
 
 	//TODO loading 컴포넌트 만들기
 	if (loading) return <p>Loading...</p>;
