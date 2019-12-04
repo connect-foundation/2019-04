e6685555341adcc36413976ef9fe970518b11fcc
YukJiSoo <wltn3231@naver.com>
2019-12-05 02:01:33 +0900

    refactor: transaction 마무리 부분 분리
    transaction 작업들을 수행하고 최종적으로 commit하고 그에
    대한 에러처리를 하는 부분을 Transaction의 prototype에 하나의 함수로
    정의해주었습니다.
    반복되는 부분이 많을 것으로 예상하여 분리했습니다.

--- api-server/src/routes/projects/files/controller.js
+++ api-server/src/routes/projects/files/controller.js
@@ -1,4 +1,4 @@
-import Transaction from 'mongoose-transactions';
+import Transaction from '../../../utils/transaction';
 import { File } from '../../../models';
 
 const FILE_MODEL_NAME = 'File';
@@ -10,7 +10,7 @@ async function createFile(req, res) {
 
 	const transaction = new Transaction();
 
-	const createFileAndUpdateChildOdParent = ({ _doc: { _id, child } }) => {
+	const createFileAndUpdateChildOfParent = ({ _doc: { _id, child } }) => {
 		const newFile = { ...newFileInfo, projectId };
 		const newFileId = transaction.insert(FILE_MODEL_NAME, newFile);
 
@@ -19,23 +19,12 @@ async function createFile(req, res) {
 
 		transaction.update(FILE_MODEL_NAME, parentFileId, newDataOfParent);
 
-		transaction
-			.run()
-			.then(() => res.sendStatus(200))
-			.catch(error => {
-				console.error(error);
-				transaction
-					.rollback()
-					.then(() => {
-						transaction.clean();
-						res.sendStatus(500);
-					})
-					.catch(() => res.sendStatus(500));
-			});
+		const successHandler = () => res.sendStatus(201);
+		transaction.runAndTerminate({ successHandler });
 	};
 
 	File.findById(parentId)
-		.then(createFileAndUpdateChildOdParent)
+		.then(createFileAndUpdateChildOfParent)
 		.catch(() => res.sendStatus(500));
 }
 
