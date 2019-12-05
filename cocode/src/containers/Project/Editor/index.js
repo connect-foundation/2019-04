a50797ca6441b28225a0e8ede43651ff16447613
YukJiSoo <wltn3231@naver.com>
2019-12-06 01:00:21 +0900

    feat: file의 code update 요청 로직 구현
    editor에서 code를 작성할 경우 변경된 사항의 갱신을 요청하는 로직을
    구현했습니다.

--- cocode/src/containers/Project/Editor/index.js
+++ cocode/src/containers/Project/Editor/index.js
@@ -1,14 +1,62 @@
 import React, { useState, useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import * as Styled from './style';
 
 import FileTabBar from 'components/Project/FileTabBar';
 import MonacoEditor from 'components/Project/MonacoEditor';
 
+import ProjectContext from 'contexts/ProjectContext';
+import {
+	updateCodeActionCreator,
+	undoUpdateCodeActionCreator
+} from 'actions/Project';
+
+import useFetch from 'hooks/useFetch';
+
+import { updateFileAPICreator } from 'apis/File';
+
 function Editor() {
+	const { projectId } = useParams();
+	const { project, dispatchProject } = useContext(ProjectContext);
+	const [code, setCode] = useState(project.editingCode);
+	const [isEditorMounted, setIsEditorMounted] = useState(false);
+	const [_, setRequest] = useFetch({});
+
+	const { selectedFileId } = project;
+
+	const handleUpdateCode = (_, changedCode) => {
+		setCode(changedCode);
+
+		const updateCodeAction = updateCodeActionCreator({
+			changedCode: changedCode
+		});
+		dispatchProject(updateCodeAction);
+	};
+
+	const handleChangedSelectedFile = () => setCode(project.editingCode);
+
+	const handleRequestUpdateCode = () => {
+		if (!isEditorMounted) return;
+		const updateFileAPI = updateFileAPICreator(projectId, selectedFileId, {
+			contents: code
+		});
+		setRequest(updateFileAPI);
+	};
+
+	useEffect(handleChangedSelectedFile, [project.selectedFileId]);
+	useEffect(handleRequestUpdateCode, [code]);
+
+	const handleEditorDidMount = () => setIsEditorMounted(true);
 	return (
 		<Styled.Editor>
 			<FileTabBar />
-			<MonacoEditor className="Stretch-width" />
+			<MonacoEditor
+				isFilesEmpty={!project.selectedFileId}
+				code={code}
+				handleUpdateCode={handleUpdateCode}
+				handleEditorDidMount={handleEditorDidMount}
+				className="Stretch-width"
+			/>
 		</Styled.Editor>
 	);
 }
