a32c4386de1597e47dde42c82885952d6f14dcc6
Basiltoast <basiltoast@gmail.com>
2019-12-06 10:09:08 +0900

    chore: 삭제되어 사용하지 못하는 코드 주석처리
    bundler의 동작방식이 변경되어 더이상 사용하지 않는 변수를 참조하는
    코드를 주석처리하였습니다.

--- cocode/src/components/Project/BrowserV1/index.js
+++ cocode/src/components/Project/BrowserV1/index.js
@@ -3,7 +3,7 @@ import * as Styled from './style';
 
 import ProjectContext from 'contexts/ProjectContext';
 // import { updateCodeActionCreator } from 'actions/Project';
-import * as bundler from 'bundler';
+// import * as bundler from 'bundler';
 
 // import * as babel from '@babel/core';
 // import reactPreset from '@babel/preset-react';
@@ -24,39 +24,39 @@ function BrowserV1({ code, ...props }) {
 	// };
 
 	// useEffect(buildCode, [files]);
-	useEffect(() => {
-		const fileTemp = {};
-		Object.keys(bundler.exports).forEach(key => {
-			delete bundler.exports[key];
-		});
-		function fileParser(id, path = '') {
-			if (files[id].type !== 'directory') {
-				fileTemp[`${path}/${files[id].name}`] = {
-					contents: files[id].contents
-				};
-				bundler.exports[`${path}/${files[id].name}`] = {
-					contents: files[id].contents
-				};
-			} else {
-				files[id].child.forEach(file => {
-					fileParser(file, `${path}/${files[id].name}`);
-				});
-			}
-		}
-		if (project) fileParser(project.root);
+	// useEffect(() => {
+	// 	const fileTemp = {};
+	// 	Object.keys(bundler.exports).forEach(key => {
+	// 		delete bundler.exports[key];
+	// 	});
+	// 	function fileParser(id, path = '') {
+	// 		if (files[id].type !== 'directory') {
+	// 			fileTemp[`${path}/${files[id].name}`] = {
+	// 				contents: files[id].contents
+	// 			};
+	// 			bundler.exports[`${path}/${files[id].name}`] = {
+	// 				contents: files[id].contents
+	// 			};
+	// 		} else {
+	// 			files[id].child.forEach(file => {
+	// 				fileParser(file, `${path}/${files[id].name}`);
+	// 			});
+	// 		}
+	// 	}
+	// 	if (project) fileParser(project.root);
 
-		setFileSystem(fileTemp);
-	}, [files]);
+	// 	setFileSystem(fileTemp);
+	// }, [files]);
 
-	useEffect(() => {
-		console.log(fileSystem);
-		try {
-			bundler.init();
-			bundler.require('/root/src/index');
-		} catch (error) {
-			console.log(error);
-		}
-	}, [fileSystem]);
+	// useEffect(() => {
+	// 	console.log(fileSystem);
+	// 	try {
+	// 		bundler.init();
+	// 		bundler.require('/root/src/index');
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 	}
+	// }, [fileSystem]);
 	return <Styled.BrowserV1 {...props}></Styled.BrowserV1>;
 }
 
