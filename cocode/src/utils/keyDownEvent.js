738daf2149e38e0bee2669caab0d873004bc8752
lallaheeee <ooop0422@gmail.com>
2019-12-11 19:18:22 +0900

    chore: 키다운 이벤트 유틸에 출처 표기
    키다운 이벤트에 출처를 표기했습니다.

--- cocode/src/utils/keyDownEvent.js
+++ cocode/src/utils/keyDownEvent.js
@@ -1,5 +1,6 @@
-import { KEY_CODE_S } from '../constants/keyCode';
+import { KEY_CODE_S } from 'constants/keyCode';
 
+//출처: https://michilehr.de/overwrite-cmds-and-ctrls-in-javascript/
 const isPressCtrlAndS = e =>
 	(window.navigator.platform.match('Mac') ? e.metaKey : e.ctrlKey) &&
 	e.which === KEY_CODE_S;
