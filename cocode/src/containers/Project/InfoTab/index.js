15cba9aef0b9afd57de520d9350a875c05086c3c
hzoou <woojooo52@naver.com>
2019-12-16 15:19:47 +0900

    feat: #38 인포 탭 프로젝트 삭제 API 연동
    프로젝트 페이지의 인포 탭 컨테이너에서
    프로젝트의 정보를 삭제하는 API를 연동했습니다.

--- cocode/src/containers/Project/InfoTab/index.js
+++ cocode/src/containers/Project/InfoTab/index.js
@@ -1,25 +1,34 @@
 import React, { useContext, useState, useRef, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import * as Styled from './style';
 import Modify from './modify.svg';
 
 import { KEY_CODE_ENTER } from 'constants/keyCode';
 import ProjectContext from 'contexts/ProjectContext';
-import { updateCoconutsAPICreator } from 'apis/DashBoard';
 import { updateProjectInfoActionCreator } from 'actions/Project';
+import useFetch from 'hooks/useFetch';
+import {
+	deleteCoconutsAPICreator,
+	updateCoconutsAPICreator
+} from 'apis/DashBoard';
 import {
 	selectAllTextAboutFocusedDom,
 	changeDivEditable
 } from 'utils/domControl';
-import useFetch from 'hooks/useFetch';
 
 const INFO_TAB_TITLE = 'COCONUT INFO';
-const OK = 200;
+const UPDATED_OK = 200;
+const DELETED_OK = 204;
+const ACCEPT_DELETE_NOTIFICATION = '해당 프로젝트를 삭제하시겠습니까?';
 
-function Info({ title, content, dispatchProject }) {
-	const { projectId } = useParams();
-	const [{ status }, setRequest] = useFetch({});
-
+function Info({
+	title,
+	content,
+	projectId,
+	status,
+	setRequest,
+	dispatchProject
+}) {
 	const input = useRef();
 	const [isEditable, setIsEditable] = useState(false);
 	const [value, setValue] = useState(content);
@@ -48,7 +57,7 @@ function Info({ title, content, dispatchProject }) {
 	};
 
 	useEffect(() => {
-		if (status === OK) {
+		if (status === UPDATED_OK) {
 			const payload = { [title]: value };
 			dispatchProject(updateProjectInfoActionCreator(payload));
 		}
@@ -64,7 +73,9 @@ function Info({ title, content, dispatchProject }) {
 						src={Modify}
 						alt="modify.svg"
 					/>
-				) : ''}
+				) : (
+					''
+				)}
 			</Styled.InfoTitleTab>
 			<div
 				ref={input}
@@ -82,8 +93,21 @@ function Info({ title, content, dispatchProject }) {
 }
 
 function InfoTab() {
+	const history = useHistory();
 	const { project, dispatchProject } = useContext(ProjectContext);
+	const [{ status }, setRequest] = useFetch({});
 	const { name, description, author } = project;
+	const { projectId } = useParams();
+
+	const handleDeleteCoconut = () => {
+		const acceptDeleteThisCoconut = confirm(ACCEPT_DELETE_NOTIFICATION);
+		if (!acceptDeleteThisCoconut) return;
+		setRequest(deleteCoconutsAPICreator(projectId));
+	};
+
+	useEffect(() => {
+		if (status === DELETED_OK) history.replace('../dashboard');
+	}, [status]);
 
 	return (
 		<>
@@ -95,21 +119,32 @@ function InfoTab() {
 					title="name"
 					content={name}
 					project={project}
+					projectId={projectId}
+					status={status}
+					setRequest={setRequest}
 					dispatchProject={dispatchProject}
 				/>
 				<Info
 					title="description"
 					content={description}
 					project={project}
+					projectId={projectId}
+					status={status}
+					setRequest={setRequest}
 					dispatchProject={dispatchProject}
 				/>
 				<Info
 					title="author"
 					content={author}
 					project={project}
+					projectId={projectId}
+					status={status}
+					setRequest={setRequest}
 					dispatchProject={dispatchProject}
 				/>
-				<Styled.Button>Delete Coconut</Styled.Button>
+				<Styled.Button onClick={handleDeleteCoconut}>
+					Delete Coconut
+				</Styled.Button>
 			</Styled.TabBody>
 		</>
 	);
