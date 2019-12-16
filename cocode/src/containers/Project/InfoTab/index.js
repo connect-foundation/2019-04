8c58acda1dbcff344905c6e90d96680361f8aa93
hzoou <woojooo52@naver.com>
2019-12-16 14:20:02 +0900

    feat: #246 인포 탭 프로젝트 수정 API 연동
    프로젝트 페이지의 인포 탭 컨테이너에서
    프로젝트의 정보를 수정하는 API를 연동했습니다.

--- cocode/src/containers/Project/InfoTab/index.js
+++ cocode/src/containers/Project/InfoTab/index.js
@@ -1,55 +1,70 @@
-import * as React from 'react';
-import { useState, useRef, useEffect } from 'react';
+import React, { useContext, useState, useRef, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import * as Styled from './style';
-import { KEY_CODE_ENTER } from 'constants/keyCode';
-
 import Modify from './modify.svg';
 
-const INFO_TAB_TITLE = 'COCONUT INFO';
+import { KEY_CODE_ENTER } from 'constants/keyCode';
+import ProjectContext from 'contexts/ProjectContext';
+import { updateCoconutsAPICreator } from 'apis/DashBoard';
+import { updateProjectInfoActionCreator } from 'actions/Project';
+import {
+	selectAllTextAboutFocusedDom,
+	changeDivEditable
+} from 'utils/domControl';
+import useFetch from 'hooks/useFetch';
 
-function Info({ title, context }) {
+const INFO_TAB_TITLE = 'COCONUT INFO';
+const OK = 200;
+
+function Info({ title, content, dispatchProject }) {
+	const { projectId } = useParams();
+	const [{ status }, setRequest] = useFetch({});
+
 	const input = useRef();
 	const [isEditable, setIsEditable] = useState(false);
-	const [value, setValue] = useState(context);
+	const [value, setValue] = useState(content);
 
 	const handleAbleEdit = () => {
 		setIsEditable(true);
-		if (input.current) input.current.focus();
+		changeDivEditable(input.current, true);
 	};
 	const handleOnChange = event => setValue(event.target.innerText);
 
-	const handleOnSubmit = () => setIsEditable(false);
+	const handleOnSubmit = event => {
+		setIsEditable(false);
+		const newContent = event.currentTarget.textContent;
+		if (content === newContent) return;
+		setValue(event.currentTarget.textContent);
+		setRequest(
+			updateCoconutsAPICreator(projectId, { [title]: newContent })
+		);
+	};
 
 	const handleOnKeyDown = event => {
 		if (event.keyCode === KEY_CODE_ENTER) {
-			handleOnSubmit();
+			handleOnSubmit(event);
 			event.preventDefault();
 		}
 	};
 
-	const handleOnFocus = event => {
-		const target = event.target;
-		const range = document.createRange();
-		const selection = window.getSelection();
-		range.setStart(target, 1);
-		range.collapse(true);
-		selection.removeAllRanges();
-		selection.addRange(range);
-		target.focus();
-	};
-
 	useEffect(() => {
-		if (isEditable) input.current.focus();
-	}, [isEditable]);
+		if (status === OK) {
+			const payload = { [title]: value };
+			dispatchProject(updateProjectInfoActionCreator(payload));
+		}
+	}, [status]);
+
 	return (
 		<Styled.Info>
 			<Styled.InfoTitleTab>
 				<h2>{title}</h2>
-				<Styled.Icon
-					onClick={handleAbleEdit}
-					src={Modify}
-					alt="modify.svg"
-				/>
+				{title !== 'author' ? (
+					<Styled.Icon
+						onClick={handleAbleEdit}
+						src={Modify}
+						alt="modify.svg"
+					/>
+				) : ''}
 			</Styled.InfoTitleTab>
 			<div
 				ref={input}
@@ -57,7 +72,7 @@ function Info({ title, context }) {
 				className={title}
 				onChange={handleOnChange}
 				onBlur={handleOnSubmit}
-				onFocus={handleOnFocus}
+				onFocus={selectAllTextAboutFocusedDom}
 				onKeyDown={handleOnKeyDown}
 			>
 				{value}
@@ -66,16 +81,34 @@ function Info({ title, context }) {
 	);
 }
 
-function InfoTab({ name, description, author }) {
+function InfoTab() {
+	const { project, dispatchProject } = useContext(ProjectContext);
+	const { name, description, author } = project;
+
 	return (
 		<>
 			<Styled.TabHeader>
 				<Styled.Title>{INFO_TAB_TITLE}</Styled.Title>
 			</Styled.TabHeader>
 			<Styled.TabBody>
-				<Info title="name" context={name} />
-				<Info title="description" context={description} />
-				<Info title="author" context={author} />
+				<Info
+					title="name"
+					content={name}
+					project={project}
+					dispatchProject={dispatchProject}
+				/>
+				<Info
+					title="description"
+					content={description}
+					project={project}
+					dispatchProject={dispatchProject}
+				/>
+				<Info
+					title="author"
+					content={author}
+					project={project}
+					dispatchProject={dispatchProject}
+				/>
 				<Styled.Button>Delete Coconut</Styled.Button>
 			</Styled.TabBody>
 		</>
