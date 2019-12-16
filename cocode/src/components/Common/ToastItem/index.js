c055ca6baca3952a70c05b35b65801c26dc755b9
lallaheeee <ooop0422@gmail.com>
2019-12-16 21:44:23 +0900

    feat: #252 알림 및 에러 메세지를 띄울 토스트 컴포넌트 구현
    토스트 컴포넌트는 기본적으로 어떤 액션에 대한 성공 여부 같이
    알림과 에러 메세지를 제공합니다. 그렇기 때문에 어느 컴포넌트에서도 사용할 수 있어야 했고,
    다른 컴포넌트에서도 쉽게 사용할 수 있어야된다는 고민을 가지고 개발했습니다.
    
    처음에는 전체 toastItem을 가진 Provider를 작성하고,
    toastItem을 더해주는 함수를 context를 이용한 방법을 시도하였으나,
    전체 App 혹은 해당 페이지를 해당 Provider로 감싸주어야하는 비용이 있었고,
    
    toastItem의 배열과 새로운 toastItem을 더하는 함수만 관리하는데에 비해
    ToastContainer와 Store의 분리하여 toast Item리스트의 상태관리를 하는 게
    과한 분리라 판단했습니다.
    
    여러 방법을 시도하다, 컴포넌트가 아닌 ToastItem를 더하는 함수만을 export하고
    그 함수에서 render를 해주는 방법으로 구현했습니다.
    
    Portal을 이용하는 방법을 시도했으나 여러 에러 메세지와 함께 에러가 났습니다.
    미래의 제가 이 커밋메세지를 다시 읽게 된다면 portal을 이용해 다시한번 구현해보았으면
    좋겠습니다.
    
    해당 함수는 다음과 같이 실행할 수 있습니다.
    import addToast from ‘components/common/ToastContainer’
    
    addToast.info(message, options)
    addToast.error(message, options)
    
    import 후 해당 함수의 info, error의 프로퍼티를 통해 돔 안에 렌더링 됩니다.
    option으로는 사라지기 전까지의 시간을 줄 수 있으며 현재 기본값은 3000ms 입니다.

---
+++ cocode/src/components/Common/ToastItem/index.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import * as Styled from './style';
+
+function ToastItem({ id, close, type, message, time }) {
+	const [toastState, setToastState] = useState({
+		isAppear: true,
+		isWillDisappear: false,
+		isDidDisappear: false
+	});
+
+	const error = type === 'error';
+
+	useEffect(() => {
+		setTimeout(() => {
+			setToastState({
+				...toastState,
+				isWillDisappear: true
+			});
+		}, time || 3000);
+	}, []);
+
+	useEffect(() => {
+		if (toastState.isWillDisappear)
+			setTimeout(() => {
+				setToastState({
+					...toastState,
+					isDidDisappear: true
+				});
+			}, 500);
+	}, [toastState.isWillDisappear]);
+
+	useEffect(() => {
+		if (!toastState.isDidDisappear) return;
+		close(id);
+		setToastState({
+			...toastState,
+			isAppear: false
+		});
+	}, [toastState.isDidDisappear]);
+
+	return (
+		<>
+			{toastState.isAppear && (
+				<Styled.Toasts
+					error={error}
+					isWillDisappear={toastState.isWillDisappear}
+				>
+					{error ? <Styled.Close /> : <Styled.InfoIcon />}
+					<Styled.MessageContainer>
+						<Styled.Text> {message} </Styled.Text>
+					</Styled.MessageContainer>
+				</Styled.Toasts>
+			)}
+		</>
+	);
+}
+
+export default ToastItem;
