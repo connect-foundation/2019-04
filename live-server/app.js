94f2b519d5f5859821fecdbae078b2f9f8402d5a
hzoou <woojooo52@naver.com>
2019-12-18 18:04:26 +0900

    feat: 소켓 룸 생성
    클라이언트에서 룸을 생성해달라는 요청이 온 경우
    프로젝트 아이디로 소켓 룸을 생성했습니다.

---
+++ live-server/app.js
@@ -0,0 +1,49 @@
+const io = require('socket.io')();
+const port = 3040;
+
+const rooms = {};
+
+io.on('connection', socket => {
+    const isNotHost = (host, username) => host.username !== username;
+    const isFirstVisit = (participants, username) => indexOfUser(participants, username) === -1;
+    const indexOfUser = (participants, username) =>
+        participants.findIndex(participant => participant.username === username);
+
+    const HandleCreateRoom = ({ projectId, user, project }) => {
+        socket.user = user;
+        socket.room = projectId;
+        socket.join(projectId);
+
+        if (rooms[projectId]) { // 방에 참여하는 경우
+            const room = rooms[socket.room];
+            const { host, participants } = room;
+            const { user: { username }} = socket;
+
+            // 호스트 혹은 이미 접속한 사람이 재접속했을 때 참가자에 추가되는 것을 방지
+            if (isNotHost(host, username) && isFirstVisit(participants, username))
+                participants.push(socket.user);
+
+            // 본인에게 알리기
+            io.to(socket.id).emit('alreadyExistRoom', { host, project });
+        } else { // 방을 생성하는 경우
+            rooms[projectId] = {};
+            rooms[projectId]['host'] = user;
+            rooms[projectId]['project'] = project;
+            rooms[projectId]['participants'] = [];
+
+            // 본인에게 알리기
+            io.to(socket.id).emit('successCreatedRoom', { project });
+        }
+
+        const room = rooms[socket.room];
+        const { participants } = room;
+
+        // 해당 방에 있는 사람들에게 알리기
+        io.in(socket.room).emit('joinUser', { participants });
+    };
+
+    socket.emit('connected');
+    socket.on('createRoom', HandleCreateRoom);
+});
+
+io.listen(port);
