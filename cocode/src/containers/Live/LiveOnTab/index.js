c741b0408ac3163d1e4dee8f88563d9b0c5fe300
hzoou <woojooo52@naver.com>
2019-12-18 17:44:06 +0900

    feat: #285 링크 클립보드로 복사
    라이브 탭에서 제공된 url을 클립보드로
    복사하는 기능을 구현했습니다.

--- cocode/src/containers/Live/LiveOnTab/index.js
+++ cocode/src/containers/Live/LiveOnTab/index.js
@@ -1,12 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
 import * as Styled from './style';
 import { LiveContext, UserContext } from 'contexts';
 
+import copy from './copy.svg';
 import close from './close.svg';
 import LiveUsers from 'components/Live/LiveUsers';
 
 import { liveOffActionCreator } from 'actions/Live';
+import { copyToClipboard } from 'utils/domControl';
 
 const LIVE_STATUS_LABEL = 'You’ve gone live!';
 const ON_BUTTON_LABEL = 'Stop Live';
@@ -14,6 +16,7 @@ const ON_DESCRIPTION =
 	'Share this link with others to invite them to the live.';
 
 function LiveOnTab() {
+	const link = useRef();
 	const history = useHistory();
 	const { projectId } = useParams();
 	const { user } = useContext(UserContext);
@@ -27,6 +30,8 @@ function LiveOnTab() {
 		history.replace(`../project/${projectId}`);
 	};
 
+	const handleCopyLink = () => copyToClipboard(link.current);
+
 	return (
 		<Styled.Container>
 			<Styled.LiveStatusLabel>
@@ -34,7 +39,8 @@ function LiveOnTab() {
 				{LIVE_STATUS_LABEL}
 			</Styled.LiveStatusLabel>
 			<Styled.Description>{ON_DESCRIPTION}</Styled.Description>
-			<Styled.LinkURL>
+			<Styled.LinkURL ref={link}>
+				<Styled.Copy src={copy} onClick={handleCopyLink} />
 				{url}
 			</Styled.LinkURL>
 			{user === owner ? (
--- cocode/src/utils/domControl.js
+++ cocode/src/utils/domControl.js
@@ -7,4 +7,12 @@ function changeDivEditable(node, status) {
 	if (status) node.focus();
 }
 
-export { selectAllTextAboutFocusedDom, changeDivEditable };
+function copyToClipboard(node) {
+	const range = document.createRange();
+	range.selectNode(node);
+	window.getSelection().removeAllRanges();
+	window.getSelection().addRange(range);
+	document.execCommand('copy');
+}
+
+export { selectAllTextAboutFocusedDom, changeDivEditable, copyToClipboard };
