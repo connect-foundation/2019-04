ded198dcad482ebc792900c16b5f2c60b83ce0c6
BasilToast <33782602+BasilToast@users.noreply.github.com>
2019-12-19 01:17:36 +0900

    feat: live-editor 기본 이벤트 추가
    라이브 에디터에서 소켓통신에 사용하는 기본적인 이벤트를 정의하였습니다.

--- cocode/src/containers/Live/Editor/index.js
+++ cocode/src/containers/Live/Editor/index.js
@@ -1,11 +1,11 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import * as Styled from './style';
 
 import FileTabBar from 'components/Project/FileTabBar';
 import MonacoEditor from 'components/Project/MonacoEditor';
 
-import { UserContext, ProjectContext } from 'contexts';
+import { LiveContext, UserContext, ProjectContext } from 'contexts';
 import {
 	updateCodeActionCreator,
 	saveFileActionCreator
@@ -16,14 +16,51 @@ import { updateFileAPICreator } from 'apis/File';
 
 import { isPressCtrlAndS } from 'utils/keyDownEvent';
 
+
+class CursorWidget {
+    constructor(editor, userName, position) {
+        this.editor = editor;
+        this.id = userName;
+        this.domNode = null;
+        this.position = position;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getDomNode() {
+        if (!this.domNode) {
+            this.domNode = document.createElement('div');
+            this.domNode.innerHTML = this.id;
+            this.domNode.style.background = 'grey';
+            this.domNode.id = this.id;
+        }
+        return this.domNode;
+    }
+
+    getPosition() {
+        return {
+            position: this.position,
+            preference: [0]
+        };
+    }
+    updatePosition(position) {
+        this.position = position;
+        this.editor.layoutContentWidget(this);
+    }
+}
+
 // Constatnts
 let timer;
 const DEBOUNCING_TIME = 800;
+const userCursor = {};
 
 function Editor({ handleForkCoconut }) {
 	const { user } = useContext(UserContext);
 	const { projectId } = useParams();
 	const { project, dispatchProject } = useContext(ProjectContext);
+	const { socket, dispatchLive } = useContext(LiveContext);
 	const [code, setCode] = useState(project.editingCode);
 	const [isEditorMounted, setIsEditorMounted] = useState(false);
 	const [_, setRequest] = useFetch({});
@@ -31,12 +68,16 @@ function Editor({ handleForkCoconut }) {
 	const [fileSelectFlag, setFileSelectFlag] = useState(undefined);
 	const { selectedFileId } = project;
 
-	const handleOnChangeCodeInMonaco = (_, changedCode) => {
-		if (timer) clearTimeout(timer);
+	const editorRef = useRef();
+	const isBusy = useRef(true);
+	const pendingEvent = useRef(false);
 
-		timer = setTimeout(() => {
-			setCode(changedCode);
-		}, DEBOUNCING_TIME);
+	const handleOnChangeCodeInMonaco = (_, changedCode) => {
+		// if (timer) clearTimeout(timer);
+
+		// timer = setTimeout(() => {
+		// 	setCode(changedCode);
+		// }, DEBOUNCING_TIME);
 	};
 
 	const handleChangedSelectedFile = () => setCode(project.editingCode);
@@ -77,10 +118,84 @@ function Editor({ handleForkCoconut }) {
 		dispatchProject(updateCodeAction);
 	};
 
+    const handleEmit = (e, timeStamp) => {
+        if (isBusy.current) return;
+        if (!timeStamp) timeStamp = Date();
+        if (pendingEvent.current) handleEmit(e, timeStamp);
+        const change = e.changes[0];
+        const operation = {
+            rangeLength: change.rangeLength,
+            rangeOffset: change.rangeOffset,
+            text: change.text.replace(/\r\n/g, '\n'),
+            timeStamp: timeStamp
+		};
+        socket.emit('change', operation);
+    };
+
+    const handleCursor = e => {
+        socket.emit('moveCursor', e.position);
+	};
+
+	const handleEditorDidMount = (_, editor) => {
+		editorRef.current = editor;
+        editor.onDidChangeModelContent(handleEmit);
+		editor.onDidChangeCursorPosition(handleCursor);
+		setIsEditorMounted(true);
+	};
+
 	useEffect(handleUpdateCode, [code]);
 	useEffect(handleChangedSelectedFile, [project.selectedFileId]);
 
-	const handleEditorDidMount = () => setIsEditorMounted(true);
+	useEffect(() => {
+		if (!socket) return;
+		isBusy.current = false;
+		socket.on('change', (socketId, op) => {
+			if (socket.id === socketId) {
+				setTimeout(() => {
+					pendingEvent.current = false;
+				}, 10);
+			} else {
+				const rangeOffset = op.rangeOffset;
+				const rangeLength = op.rangeLength;
+				const text = op.text;
+
+				const startPosition = editorRef.current
+					.getModel()
+					.getPositionAt(rangeOffset);
+				const endPosition = editorRef.current
+					.getModel()
+					.getPositionAt(rangeOffset + rangeLength);
+				isBusy.current = true;
+				editorRef.current.executeEdits(socketId, [
+					{
+						range: {
+							startLineNumber: startPosition.lineNumber,
+							startColumn: startPosition.column,
+							endLineNumber: endPosition.lineNumber,
+							endColumn: endPosition.column
+						},
+						text,
+						forceMoveMarkers: true
+					}
+				]);
+				isBusy.current = false;
+			}
+		});
+
+		socket.on('moveCursor', (username, position) => {
+			if (!userCursor[username]) {
+				const widget = new CursorWidget(
+					editorRef.current,
+					username,
+					position
+				);
+				userCursor[username] = widget;
+				editorRef.current.addContentWidget(widget);
+				return;
+			}
+			userCursor[username].updatePosition(position);
+		});
+	}, [socket]);
 
 	return (
 		<Styled.Editor>
@@ -96,5 +211,6 @@ function Editor({ handleForkCoconut }) {
 		</Styled.Editor>
 	);
 }
+
 
 export default Editor;
