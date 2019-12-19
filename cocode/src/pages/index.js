eb8255cbfe1924f0904dac0ce9c64bfeaf95a723
hzoou <woojooo52@naver.com>
2019-12-20 03:23:25 +0900

    feat: #303 원래 url로 리다이렉트 시켜주는 페이지 구현
    로그인 성공 시 로컬 스토리지에 저장되어있던 원래 url로
    리다이렉트 시켜주는 페이지를 구현했습니다.

--- cocode/src/pages/index.js
+++ cocode/src/pages/index.js
@@ -4,5 +4,6 @@ import Project from './Project';
 import NotFound from './NotFound';
 import Live from './Live';
 import SignIn from './SignIn';
+import Empty from './Empty';
 
-export { Home, DashBoard, Project, NotFound, Live, SignIn };
+export { Home, DashBoard, Project, NotFound, Live, SignIn, Empty };
