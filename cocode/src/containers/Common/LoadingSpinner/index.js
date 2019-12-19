e8da678fb000b9afb23db416f28def41915e5a09
lallaheeee <ooop0422@gmail.com>
2019-12-19 17:34:05 +0900

    refactor: 로딩 스피너 컴포넌트로 분리
    기존의 페이지 이동에 필요한 로딩 스피너가 중복되어 컨테이너 컴포넌트로 분리했습니다.

---
+++ cocode/src/containers/Common/LoadingSpinner/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as Styled from './style';
+import CoconutSpinner from 'components/Common/CoconutSpinner';
+
+function LoadingSpinner(message) {
+	return (
+		<Styled.LoadingDisplay>
+			<CoconutSpinner />
+			<Styled.LoadingPhrase>{message}</Styled.LoadingPhrase>
+		</Styled.LoadingDisplay>
+	);
+}
+
+export default LoadingSpinner;
