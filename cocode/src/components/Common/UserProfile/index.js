171c69564af6fb433c97d130bfc7f0f50e9f1acf
hzoou <woojooo52@naver.com>
2019-12-22 18:18:45 +0900

    chore: 헤더 프로필 우측에 화살표 추가
    해당 영역을 클릭하라는 인터렉션을 위해
    헤더 프로필 우측에 화살표를 추가했습니다.

--- cocode/src/components/Common/UserProfile/index.js
+++ cocode/src/components/Common/UserProfile/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import * as Styled from './style';
+
 import DropDownMenu from 'components/Common/DropDownMenu';
+import down from './down.svg';
 
 function UserProfile({ username, avatar, menuItems }) {
 	return (
 		<Styled.UserProfile>
 			<Styled.UserName>{username}</Styled.UserName>
+			<Styled.UserAvatar src={avatar} alt="프로필" />
 			<DropDownMenu menuItems={menuItems}>
-				<Styled.UserAvatar src={avatar} alt="프로필" />
+				<Styled.DownArrow src={down} />
 			</DropDownMenu>
 		</Styled.UserProfile>
 	);
